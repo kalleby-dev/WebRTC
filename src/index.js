6a9a1a6fa980480262effe284589a9c9c1131796
kalleby <kalleby.santos38@gmail.com>
2020-10-21 23:06:49 +0100

    feat: client setup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ const app = express();
 const server = require('http').createServer(app);
 // const socket = sockio(server);
 
+app.set('view engine', 'ejs');
+app.set('views', './client/views');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+  res.render('index');
 });
 
 server.listen(8000, () => console.log('Server is listening on port 8000'));
